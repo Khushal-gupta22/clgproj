2cec165e0791244454bd4794314d59ecc4b90303
Khushal-gupta22 <106315796+Khushal-gupta22@users.noreply.github.com>
2022-11-09 02:26:58 +0530

    added changes

--- hospital website/js/script.js
+++ hospital website/js/script.js
@@ -10,3 +10,8 @@ window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
+
+let bookEl = document.getElementById("book-el");
+function book() {
+    bookEl.textContent = "Congrats! Appointment booked";
+}
